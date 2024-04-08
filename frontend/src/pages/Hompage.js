73455ba7e4fddba0d75721a8b144f757958fd43b
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-04-08 15:53:51 -0700

    create direct call component

---
+++ frontend/src/pages/Hompage.js
@@ -0,0 +1,10 @@
+export default function HomePage() {
+
+    return (
+        <>
+            <div>
+                <Link to="/new-call"><button>Make a new call</button></Link>
+            </div>
+        </>
+    );
+}
