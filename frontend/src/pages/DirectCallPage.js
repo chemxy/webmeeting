c9819396bb17cbe51f20e8232b01df81de7fcdd5
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-04-09 10:29:43 -0700

    Update DirectCallPage.js

--- frontend/src/pages/DirectCallPage.js
+++ frontend/src/pages/DirectCallPage.js
@@ -6,52 +6,38 @@ export default function DirectCallPage() {
 
     const videoRef = useRef(null);
     const [isNewCall, setNewCall] = useState(true);
-    const navigate = useNavigate();
+    const [callerId, setCallerId] = useState("");
+    const [stream, setStream] = useState();
+    const [errorMessage, setErrorMessage] = useState("");
 
-    // Put variables in global scope to make them available to the browser console.
-    const constraints = window.constraints = {
-        audio: false,
-        video: true
-    };
-
-    function handleSuccess(stream) {
-        // const video = document.querySelector('video');
-        const video = videoRef.current;
-        const videoTracks = stream.getVideoTracks();
-        console.log('Got stream with constraints:', constraints);
-        console.log(`Using video device: ${videoTracks[0].label}`);
-        window.stream = stream; // make variable available to browser console
-        video.srcObject = stream;
+    function setUserVideo(stream) {
+        setStream(stream);
+        videoRef.current.srcObject = stream;
     }
 
     function handleError(error) {
         if (error.name === 'OverconstrainedError') {
-            const v = constraints.video;
-            errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
+            setErrorMessage(`The resolution is not supported by your device.`);
         } else if (error.name === 'NotAllowedError') {
-            errorMsg('Permissions have not been granted to use your camera and ' +
+            setErrorMessage('Permissions have not been granted to use your camera and ' +
                 'microphone, you need to allow the page access to your devices in ' +
                 'order for the demo to work.');
         }
-        errorMsg(`getUserMedia error: ${error.name}`, error);
+        setErrorMessage(`getUserMedia error: ${error.name}`, error);
     }
 
-    function errorMsg(msg, error) {
-        const errorElement = document.querySelector('#errorMsg');
-        errorElement.innerHTML += `<p>${msg}</p>`;
-        if (typeof error !== 'undefined') {
-            console.error(error);
-        }
-    }
-
-    async function init(e) {
+    async function initVideo(event) {
         try {
+            const constraints = {
+                audio: true,
+                video: true
+            };
             const stream = await navigator.mediaDevices.getUserMedia(constraints);
             console.log(stream);
-            handleSuccess(stream);
-            e.target.disabled = true;
-        } catch (e) {
-            handleError(e);
+            setUserVideo(stream);
+            event.target.disabled = true;
+        } catch (err) {
+            handleError(err);
         }
     }
 
@@ -82,9 +68,9 @@ export default function DirectCallPage() {
             <div>
                 <div className="camera-wrapper">
                     <video id="gum-local" autoPlay playsInline ref={videoRef}></video>
-                    <button id="showVideo" onClick={(e) => init(e)}>Open camera</button>
+                    <button id="showVideo" onClick={(e) => initVideo(e)}>Open camera</button>
 
-                    <div id="errorMsg"></div>
+                    <div id="errorMsg">{errorMessage}</div>
                 </div>
                 <div>
                     <button onClick={hangUpCall} className="text-capitalize">end call</button>
